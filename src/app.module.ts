1b61d517dd13c59b4a1c7808c184b05b96bbcab0
Markmhbr <markmhbr@example.com>
2026-05-16 21:30:30 +0700

    Backend-simak

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, HttpAdapterHost } from '@nestjs/core';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaClientExceptionFilter } from './common/filters/prisma-client-exception.filter';
+import { validate } from './config/env.validation';
+import { PrismaModule } from './core/prisma/prisma.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate,
+    }),
+    PrismaModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useFactory: ({ httpAdapter }: HttpAdapterHost) => {
+        return new PrismaClientExceptionFilter(httpAdapter);
+      },
+      inject: [HttpAdapterHost],
+    },
+  ],
+})
+export class AppModule {}
