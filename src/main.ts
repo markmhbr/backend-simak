6585146ff4f4a5a80de730db47803f66a2bacc23
Markmhbr <mif@gmail.com>
2026-06-04 16:54:53 +0700

    Perbaikan

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ limit: '50mb', extended: true }));
   
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 0;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
 }
